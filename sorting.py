73ddf925c91c50bd9076b089d926569ea21019cc
JaresovaAdela <adelanemcice@gmail.com>
2025-04-14 09:30:39 +0200

    update

--- sorting.py
+++ sorting.py
@@ -22,9 +22,57 @@ def read_data(file_name):
                     data[header].append(int(value))
     return data
 
+def selection_sort(num_seznam, direction = "ascending"):
+    """
+    :param list num_seznam: list with numeric array
+    :param str direction: string indicating sorting direction: ascending, descending
+    :return: sorted numeric array
+    """
+    n = len(num_seznam)
+    for i in range(n):
+        min_max_i = i
+        for num_idx in range(i+1, n):
+            if direction == "ascending":
+                if num_seznam[num_idx] < num_seznam[min_max_i]:
+                    min_max_i = num_idx
+            elif direction == "descending":
+                if num_seznam[num_idx] > num_seznam[min_max_i]:
+                    min_max_i = num_idx
+        num_seznam[i], num_seznam[min_max_i] = num_seznam[min_max_i], num_seznam[i]
+    return num_seznam
+
+def bubble_sort(num_seznam):
+    """
+    :param list num_seznam: list with numeric array
+    :return: sorted numeric array
+    """
+    n = len(num_seznam)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if num_seznam[j] > num_seznam[j+1]:
+                num_seznam[j], num_seznam[j+1] = num_seznam[j+1], num_seznam[j]
+    return num_seznam
+
+def insertion_sort(num_seznam):
+    n = len(num_seznam)
+    for i in range(1, n):
+        insert_index = i
+        current_value = num_seznam.pop(i)
+        for j in range(i - 1, -1, -1):
+            if num_seznam[j] > current_value:
+                insert_index = j
+        num_seznam.insert(insert_index, current_value)
+    return num_seznam
+
 def main():
     data = read_data("numbers.csv")
     print(data)
+    selection = selection_sort(data["series_1"])
+    print(selection)
+    bubble = bubble_sort(data["series_2"])
+    print(bubble)
+    insertion = insertion_sort(data["series_3"])
+    print(insertion)
     pass
 
 
